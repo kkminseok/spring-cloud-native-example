2b119194147d8420027ea5a2bff1a7db5a36541d
kms <aaaa727@khu.ac.kr>
2024-07-08 00:11:18 +0900

    Chapter4 설정 서버 생성

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/HelloController.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/HelloController.java
@@ -1,13 +1,21 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private final PolarProperties polarProperties;
+
+    public HelloController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/")
-    public String getGreeting(){
-        return "도서 카탈로그에 오신것을 환영합니다.";
+    public String getGreeting() {
+        System.out.println(polarProperties.getGreeting());
+        return polarProperties.getGreeting();
     }
 }
