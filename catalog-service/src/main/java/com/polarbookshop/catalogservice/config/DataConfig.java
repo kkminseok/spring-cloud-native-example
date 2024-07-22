8d50dbc06fae76d4f0fd20b3871837f0b896313f
minseok-kang727 <minseok.kang727@nhn.com>
2024-07-22 19:00:59 +0900

    6

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/config/DataConfig.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/config/DataConfig.java
@@ -1,6 +1,5 @@
 package com.polarbookshop.catalogservice.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 
