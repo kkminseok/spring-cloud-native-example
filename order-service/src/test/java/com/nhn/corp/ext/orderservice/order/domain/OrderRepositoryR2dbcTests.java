d3abd449b98dc46fa5b080a3f64273e8227f8921
kms <aaaa727@khu.ac.kr>
2024-07-31 23:28:41 +0900

    [Chapter8] Docker Flux Test작성

---
+++ order-service/src/test/java/com/nhn/corp/ext/orderservice/order/domain/OrderRepositoryR2dbcTests.java
@@ -0,0 +1,54 @@
+package com.nhn.corp.ext.orderservice.order.domain;
+
+import com.nhn.corp.ext.orderservice.config.DataConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import reactor.test.StepVerifier;
+
+@DataR2dbcTest // R2DBC 컴포넌트에 집중하는 테스트 클래스임을 나타냄
+@Import(DataConfig.class)// 감사 설정
+@Testcontainers//테스트 컨테이너의 자동 시작과 중지를 활성화
+class OrderRepositoryR2dbcTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.4"));
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @DynamicPropertySource//테스트 PostgreSQL 인스턴스에 연결하도록 설정을 변경
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", OrderRepositoryR2dbcTests::r2dbcUrl);
+        registry.add("spring.r2dbc.username", postgresql::getUsername);
+        registry.add("spring.r2dbc.password", postgresql::getPassword);
+        registry.add("spring.flyway.url", postgresql::getJdbcUrl);
+    }
+
+    private static String r2dbcUrl() {
+        return String.format("r2dbc:postgresql://%s:%s/%s", postgresql.getHost(),
+                postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT), postgresql.getDatabaseName());
+    }
+
+    @Test
+    void findOrderByIdWhenNotExisting() {
+        StepVerifier.create(orderRepository.findById(394L))
+                .expectNextCount(0)
+                .verifyComplete();
+    }
+
+    @Test
+    void createRejectedOrder() {
+        var rejectedOrder = OrderService.buildRejectedOrder( "1234567890", 3);
+        StepVerifier.create(orderRepository.save(rejectedOrder))
+                .expectNextMatches(order -> order.status().equals(OrderStatus.REJECTED))
+                .verifyComplete();
+    }
+}
