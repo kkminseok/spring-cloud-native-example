bc9ad08392d5e3b3cd844f691dc79dc92b5d7027
kms <aaaa727@khu.ac.kr>
2024-08-04 17:05:39 +0900

    8장 숙제 완료

---
+++ order-service/src/main/java/com/nhn/corp/ext/orderservice/config/PolarProperties.java
@@ -0,0 +1,20 @@
+package com.nhn.corp.ext.orderservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "polar")
+public class PolarProperties {
+    /**
+     * A message to welcome users.
+     */
+    private String greeting;
+
+    public String getGreeting() {
+        return this.greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+}
+
