2fafc9251fc30788056a8527719520f658f71dca
minseok-kang727 <minseok.kang727@nhn.com>
2024-08-14 18:24:24 +0900

    #9 9장 내용 완료

--- edge-service/src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
+++ edge-service/src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -2,12 +2,33 @@ package com.polarbookshop.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@Testcontainers // 테스트 컨테이너 자동시작 ,종료 알림
 class EdgeServiceApplicationTests {
 
+    private static final int REDIS_PORT = 6379;
+
+    // 테스트를 위한 레디스 컨테이너 정의
+    @Container
+    static GenericContainer<?> redisContainer = new GenericContainer<>("redis:7.0")
+            .withExposedPorts(REDIS_PORT);
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.host", () -> redisContainer.getHost());
+        registry.add("spring.redis.port", () -> redisContainer.getMappedPort(REDIS_PORT));
+    }
+
     @Test
-    void contextLoads() {
+    void verifyThatSpringContectLoads() {
     }
 
 }
