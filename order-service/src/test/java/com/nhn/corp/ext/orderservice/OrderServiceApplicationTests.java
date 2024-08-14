17322146cded9ac214bb7bd372eee3b694f46cd5
minseok-kang727 <minseok.kang727@nhn.com>
2024-08-14 19:30:03 +0900

    fix order service test code 보수작업

--- order-service/src/test/java/com/nhn/corp/ext/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/nhn/corp/ext/orderservice/OrderServiceApplicationTests.java
@@ -2,12 +2,10 @@ package com.nhn.corp.ext.orderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("integration")
 class OrderServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
