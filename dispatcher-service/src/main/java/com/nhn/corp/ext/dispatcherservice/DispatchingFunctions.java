a996cb60e6257fec7e4a7eae56fb3fea69e93aba
minseok-kang727 <minseok.kang727@nhn.com>
2024-08-16 18:53:06 +0900

    chapter10 init, 의존성 해결 필요

---
+++ dispatcher-service/src/main/java/com/nhn/corp/ext/dispatcherservice/DispatchingFunctions.java
@@ -0,0 +1,32 @@
+package com.nhn.corp.ext.dispatcherservice;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.slf4j.Logger;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
+
+@Configuration
+public class DispatchingFunctions {
+
+    private static final Logger log = LoggerFactory.getLogger(DispatchingFunctions.class);
+
+    @Bean
+    public Function<OrderAcceptedMessage, Long> pack() {
+        return orderAcceptedMessage -> {
+            log.info("포장이 완료되었습니다. 주문 Id :{}", orderAcceptedMessage.orderId());
+            return orderAcceptedMessage.orderId();
+        };
+    }
+
+    @Bean
+    public Function<Flux<Long>, Flux<OrderDispatchedMessage>> label() {
+        return orderFlux -> orderFlux.map(orderId -> {
+            log.info("라벨링이 완료되었습니다. 주문 Id: {}", orderId);
+            return new OrderDispatchedMessage(orderId);
+        });
+    }
+}
