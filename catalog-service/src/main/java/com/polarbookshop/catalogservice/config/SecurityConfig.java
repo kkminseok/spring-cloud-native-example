f4fdc19d8b860aa632afe47c11b9ce78cd9dd23e
minseok-kang727 <minseok.kang727@nhn.com>
2024-11-20 16:33:23 +0900

    #12 security6.x에서는 @Configuration을 붙여줘야함. 3시간 삽질.. 분리가 되었음.

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/config/SecurityConfig.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.polarbookshop.catalogservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.csrf.CsrfToken;
+import org.springframework.web.server.WebFilter;
 
+//6.x에서는 붙여줘야함
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
@@ -21,7 +28,8 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
+
 }
