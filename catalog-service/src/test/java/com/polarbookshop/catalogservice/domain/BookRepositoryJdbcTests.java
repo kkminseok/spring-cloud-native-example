1957a980d84359b0f4a97fdd9930e4da766c2290
kms <aaaa727@khu.ac.kr>
2024-12-08 19:29:53 +0900

    chapter13 complete

--- catalog-service/src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTests.java
+++ catalog-service/src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTests.java
@@ -1,57 +1,57 @@
-package com.polarbookshop.catalogservice.domain;
-
-import com.polarbookshop.catalogservice.config.DataConfig;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.AutoConfigureDataJdbc;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
-import org.springframework.test.context.ActiveProfiles;
-
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@DataJdbcTest
-@Import(DataConfig.class)
-@AutoConfigureTestDatabase(
-        replace = AutoConfigureTestDatabase.Replace.NONE
-)
-@ActiveProfiles("integration")
-class BookRepositoryJdbcTests {
-
-    @Autowired
-    private BookRepository bookRepository;
-
-    @Autowired
-    private JdbcAggregateTemplate jdbcAggregateTemplate;
-
-    @Test
-    void findBookByIsbnWhenExisting() {
-        var bookIsbn = "1234561237";
-        var book = Book.of(bookIsbn, "title","author",3.3, "kms");
-        jdbcAggregateTemplate.insert(book);
-        Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
-        assertThat(actualBook).isPresent();
-        assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
-    }
-
-    @Test
-    void findAllBooks() {
-        var book1 = Book.of("1234561237", "title","author",3.3, "kms");
-        var book2 = Book.of("1234561247", "another title","author",3.3, "kms");
-        jdbcAggregateTemplate.insert(book1);
-        jdbcAggregateTemplate.insert(book2);
-
-        Iterable<Book> books = bookRepository.findAll();
-
-        assertThat(StreamSupport.stream(books.spliterator(), true)
-                .filter(book -> book.isbn().equals(book1.isbn()) || book.isbn().equals(book2))
-                .collect(Collectors.toList())).hasSize(2);
-    }
-
-}
+//package com.polarbookshop.catalogservice.domain;
+//
+//import com.polarbookshop.catalogservice.config.DataConfig;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.data.jdbc.AutoConfigureDataJdbc;
+//import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.context.annotation.Import;
+//import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//import java.util.Optional;
+//import java.util.stream.Collectors;
+//import java.util.stream.StreamSupport;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@DataJdbcTest
+//@Import(DataConfig.class)
+//@AutoConfigureTestDatabase(
+//        replace = AutoConfigureTestDatabase.Replace.NONE
+//)
+//@ActiveProfiles("integration")
+//class BookRepositoryJdbcTests {
+//
+//    @Autowired
+//    private BookRepository bookRepository;
+//
+//    @Autowired
+//    private JdbcAggregateTemplate jdbcAggregateTemplate;
+//
+//    @Test
+//    void findBookByIsbnWhenExisting() {
+//        var bookIsbn = "1234561237";
+//        var book = Book.of(bookIsbn, "title","author",3.3, "kms");
+//        jdbcAggregateTemplate.insert(book);
+//        Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
+//        assertThat(actualBook).isPresent();
+//        assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
+//    }
+//
+//    @Test
+//    void findAllBooks() {
+//        var book1 = Book.of("1234561237", "title","author",3.3, "kms");
+//        var book2 = Book.of("1234561247", "another title","author",3.3, "kms");
+//        jdbcAggregateTemplate.insert(book1);
+//        jdbcAggregateTemplate.insert(book2);
+//
+//        Iterable<Book> books = bookRepository.findAll();
+//
+//        assertThat(StreamSupport.stream(books.spliterator(), true)
+//                .filter(book -> book.isbn().equals(book1.isbn()) || book.isbn().equals(book2))
+//                .collect(Collectors.toList())).hasSize(2);
+//    }
+//
+//}
